65b8467a64a1ebcb21ed1eb15ffe47e20fdd36b5
Nauman Shahid <nauman555@gmail.com>
2023-03-01 02:56:00 +0800

    error handling and other updates

--- spreadsheet_gmail.py
+++ spreadsheet_gmail.py
@@ -10,41 +10,44 @@ subject = input("Enter the email subject: ")
 
 try:
     # establishing connection with gmail
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server.ehlo()
-    server.login(your_email, your_password)
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
+        server.ehlo()
+        server.login(your_email, your_password)
 
-    # reading the spreadsheet
-    email_list = pd.read_excel('student_emails.xlsx')
+        # reading the spreadsheet
+        email_list = pd.read_excel('student_emails.xlsx')
 
-    # getting the names and the emails
-    names = email_list['Message']
-    emails = email_list['Email']
+        # getting the names and the emails
+        names = email_list['Message']
+        emails = email_list['Email']
 
-    # iterate through the records
-    total_emails = len(emails)
-    sent_emails = 0
+        # iterate through the records
+        total_emails = len(emails)
+        sent_emails = 0
 
-    for i in range(len(emails)):
-        # for every record get the name and the email addresses
-        name = names[i]
-        email = emails[i]
+        for i, email in enumerate(emails):
+            # for every record get the name and the email addresses
+            name = names[i]
 
-        # the message to be emailed
-        message = "Subject: {}\n\n{}".format(subject, name).encode('utf-8')
+            # the message to be emailed
+            message = f"Subject: {subject}\n\n{name}".encode('utf-8')
 
-        # sending the email
-        server.sendmail(your_email, [email], message)
-        sent_emails += 1
-        print("{} emails sent out of {}".format(sent_emails, total_emails))
-        print("Email sent to: {}".format(email))
+            # sending the email
+            try:
+                server.sendmail(your_email, [email], message)
+            except smtplib.SMTPException as e:
+                print(f"An error occurred while sending email to {email}: {e}")
+                continue
 
-        # adding a random delay between 20 and 60 seconds before sending the next email
-        delay = random.randint(20, 60)
-        time.sleep(delay)
+            sent_emails += 1
+            print(f"{sent_emails} emails sent out of {total_emails}")
+            print(f"Email sent to: {email}")
 
-    # close the smtp server
-    server.close()
+            # adding a random delay between 20 and 60 seconds before sending the next email
+            delay = random.randint(20, 60)
+            time.sleep(delay)
+
+    print("All emails sent successfully")
 
 except Exception as e:
     print("An error occurred: ", e)
